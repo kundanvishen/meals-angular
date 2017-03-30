138088428da69cafcb5f9c174104f7e66c18e85f
Kundan Singh <kundan.singh979@gmail.com>
2017-03-31 04:48:33 +0530

    yo-scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mealsAngularApp
+ * @description
+ * # mealsAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mealsAngularApp', []);
